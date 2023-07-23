61cb92028b4c51a85851812ca19147e5b379a3e0
shih2000 <140154457+shih2000@users.noreply.github.com>
2023-07-23 18:40:11 +0800

    0640

--- E.sunbank0721/Pages/Orders/PayPage.cshtml.cs
+++ E.sunbank0721/Pages/Orders/PayPage.cshtml.cs
@@ -4,31 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using E.sunbank0721.Data;
 using E.sunbank0721.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace E.sunbank0721.Pages.Orders
 {
-    public class CheckOrdersModel : PageModel
+    public class PayPageModel : PageModel
     {
         private readonly E.sunbank0721.Data.Esunbank0721Context _context;
-
-        public CheckOrdersModel(E.sunbank0721.Data.Esunbank0721Context context)
+        public PayPageModel(E.sunbank0721.Data.Esunbank0721Context context)
         {
             _context = context;
         }
+        public IActionResult OnGet()
+        {
+             return Page();
+        }
 
         [BindProperty]
-        public IList<Order> Order { get; set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            if (_context.Order != null)
-            {
-                Order = await _context.Order.ToListAsync();
-            }
-        }
+        public Order Order{ get; set; } = default!;
     }
 }
