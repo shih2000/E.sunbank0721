7619f37dfc49da6becd7ec8320b54d78298eb19a
shih2000 <140154457+shih2000@users.noreply.github.com>
2023-07-23 21:54:02 +0800

    加入查詢訂單號碼box，unsuccess

---
+++ E.sunbank0721/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+using E.sunbank0721.Data;
+using E.sunbank0721.Models;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace E.sunbank0721.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly E.sunbank0721.Data.Esunbank0721Context _context;
+
+        public OrdersController(E.sunbank0721.Data.Esunbank0721Context context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string searchString)
+        {
+
+
+            var orders = from m in _context.Order
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                orders = orders.Where(s => s.OrderID.Contains(searchString));
+            }
+            return View(await orders.ToListAsync());
+        }
+    }
+}
--- E.sunbank0721/Models/Order.cs
+++ E.sunbank0721/Models/Order.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Xml.Linq;
 
 namespace E.sunbank0721.Models
