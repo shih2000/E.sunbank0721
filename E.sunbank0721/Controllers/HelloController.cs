d98fe43bf59f82389006d08cbcc2a598b2f10cac
shih2000 <140154457+shih2000@users.noreply.github.com>
2023-07-23 19:36:44 +0800

    0736

---
+++ E.sunbank0721/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace E.sunbank0721.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
